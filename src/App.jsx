2306ad0b043c0bb7419b8597205dc4d86472c8f6
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-08-27 20:45:47 +0400

    add: locomotive smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import Cards from './components/Cards'
-import Footer from './components/Footer'
-import Marquee from './components/Marquees'
-import Navbar from './components/Navbar'
-import Products from './components/Products'
-import Stripes from './components/Stripes'
-import Work from './components/Work'
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Marquee from "./components/Marquees";
+import Navbar from "./components/Navbar";
+import Products from "./components/Products";
+import Stripes from "./components/Stripes";
+import Work from "./components/Work";
+import LocomotiveScroll from "locomotive-scroll";
+
 const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className="min-h-screen w-full bg-zinc-900">
-      <Navbar/>
-      <Work/>
-      <Stripes/>
-      <Products/>
-      <Marquee/>
-      <Cards/>
-      <Footer/>
+      <Navbar />
+      <Work />
+      <Stripes />
+      <Products />
+      <Marquee />
+      <Cards />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
